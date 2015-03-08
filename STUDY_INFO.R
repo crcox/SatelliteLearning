5a108bdebc7f02c7d87dd3a19ef708a2a1d146cf
Chris Cox <crcox@wisc.edu>
2015-03-08 17:57:53 -0500

    tmp

--- STUDY_INFO.R
+++ STUDY_INFO.R
@@ -1,4 +1,6 @@
 STUDY_INFO = list(
-  datadir="/mnt/RogersLab/Experiments/Behavioral/satelliteLearning/Spring2015/data"
+  datadir="Spring2015/data"
+#   datadir="/Volumes/Data/Experiments/Behavioral/satelliteLearning/Spring2015/data"
+  #datadir="/mnt/RogersLab/Experiments/Behavioral/satelliteLearning/Spring2015/data"
   )
 save(STUDY_INFO, file='STUDY_INFO.Rdata')
--- loaddata.R
+++ loaddata.R
@@ -1,7 +1,9 @@
-loaddata <- function(round,STUDY_INFO=STUDY_INFO) {
+load_session_data <- function(round,STUDY_INFO=STUDY_INFO) {
   require(stringr)
   datadir = file.path(STUDY_INFO$datadir,sprintf('round%02d',round))
   filelist = list.files(datadir)
+  MetaData <- list()
+  SessionData <- data.frame(subject=numeric(),session=numeric(),stage=factor(NULL,levels=1:3,labels=c('study','train','test')),start=as.Date(character()),end=as.Date(character()),accuracy=numeric())
   for (f in filelist) {
     print(f)
     fileparts <- strsplit(f,'_')[[1]]
@@ -10,11 +12,92 @@ loaddata <- function(round,STUDY_INFO=STUDY_INFO) {
     fpath <- file.path(datadir,f)
     if (filetype == "session") {
       SESSION <- scan(fpath,what=character(),sep='\n')
+      
+      ## Count sessions completed
+      sesix <- grep('Session number:',SESSION)
+      nsessions <- length(sesix)
+      print(nsessions)
+      SESSIONLIST <- list()
+      sesix <- append(sesix,length(SESSION)+1)
+      for (i in 1:nsessions) {
+        SESSIONLIST[[i]] <- SESSION[sesix[i]:sesix[i+1]-1]
+      }
+      
+      ## Extract Bindings
       ix <- match('Binding matrix:',SESSION)
       Binding<-SESSION[(ix+1):(ix+6)]
       BindingListStr <- str_extract_all(Binding,'[0-9]+')
       BindingMatrix <- t(sapply(BindingListStr,as.numeric))[,-1]
       ix <- grep('Code name bindings',SESSION)[1]
+      CodeNameBindings <- as.numeric(str_extract_all(SESSION[ix],'[0-9]+')[[1]])
+      
+      ## SESSION 1
+      # Extract Study Round start/stop times
+      ix <- grep('Start study round',SESSIONLIST[[1]])[1] # Round 0
+      DateTimeString <- str_trim(str_split(SESSIONLIST[[1]][ix], pattern=':', n=2)[[1]][2])
+      StudyDateTime <- strptime(DateTimeString,'%m/%d/%Y %I:%M:%S %p')
+
+      # Extract Training (one-missing) start times
+      ix <- grep('Start inference with 1 parts missing',SESSIONLIST[[1]])
+      temp <- str_split(SESSIONLIST[[1]][ix], pattern=' at ', n=2)
+      DateTimeStrings <- sapply(temp, str_trim)[2,]
+      TrainingDateTimes_start <- strptime(DateTimeStrings,'%m/%d/%Y %I:%M:%S %p')
+      
+      # Extract Training (one-missing) stop times
+      ix <- grep('Completed  [0-9]+ trials with 1 feature/s missing',SESSIONLIST[[1]])
+      temp <- str_split(SESSIONLIST[[1]][ix], pattern=' at ', n=2)
+      DateTimeStrings <- sapply(temp, str_trim)[2,]
+      DateTimeStrings <- str_replace(DateTimeStrings,'.[0-9]+$','')
+      TrainingDateTimes_end <- strptime(DateTimeStrings,'%d-%b-%Y %H:%M:%S')
+      nrounds <- length(TrainingDateTimes_end)
+      
+      # Extract Training (one-missing) proportion correct
+      ProportionCorrectTraining <- c(NA,sapply(str_extract_all(SESSIONLIST[[1]][ix+1],'[0-1].[0-9]+'),as.numeric))
+      
+      print(factor(rep(1:2,c(1,nrounds)),levels=1:3,labels=c('study','train','test')))
+      print(c(StudyDateTime,TrainingDateTimes_start))
+      print(c(TrainingDateTimes_start[1],TrainingDateTimes_end))
+      print(as.numeric(ProportionCorrectTraining))
+      X <- data.frame(
+        subject=subjectnumber,
+        session=1,
+        stage=factor(rep(1:2,c(1,nrounds)),levels=1:3,labels=c('study','train','test')),
+        start=c(StudyDateTime,TrainingDateTimes_start),
+        end=c(TrainingDateTimes_start[1],TrainingDateTimes_end),
+        accuracy=as.numeric(ProportionCorrectTraining)
+      )
+      SessionData <- rbind(SessionData,X)
+      
+      ## SESSIONS 2 & 3 (possibly more or less in case of errors)
+      # Extract Testing (two-missing) start times
+      for (i in 2:nsessions) {
+        ix <- grep('Start inference with 2 parts missing',SESSIONLIST[[i]])
+        if (length(ix) == 0) {next}
+        temp <- str_split(SESSIONLIST[[i]][ix], pattern=' at ', n=2)
+        DateTimeStrings <- sapply(temp, str_trim)[2,]
+        TestDateTimes_start <- strptime(DateTimeStrings,'%m/%d/%Y %I:%M:%S %p')
+        
+        # Extract Training (two-missing) stop times
+        ix <- grep('Completed  [0-9]+ trials with 2 feature/s missing',SESSIONLIST[[i]])
+        temp <- str_split(SESSIONLIST[[i]][ix], pattern=' at ', n=2)
+        DateTimeStrings <- sapply(temp, str_trim)[2,]
+        DateTimeStrings <- str_replace(DateTimeStrings,'.[0-9]+$','')
+        TestDateTimes_end <- strptime(DateTimeStrings,'%d-%b-%Y %H:%M:%S')
+        ntests <- length(ix)
+        # Extract Training (two-missing) proportion correct
+        ProportionCorrectTest <- sapply(str_extract_all(SESSIONLIST[[i]][ix+1],'[0-1].[0-9]+'),as.numeric)
+        
+        X <- data.frame(
+          subject=subjectnumber,
+          session=i,
+          stage=factor(3,levels=1:3,labels=c('study','train','test')),
+          start=TestDateTimes_start,
+          end=TestDateTimes_end,
+          accuracy=ProportionCorrectTest
+        )
+        SessionData <- rbind(SessionData,X)
+      }
     }
   }
+  return(SessionData)
 }
